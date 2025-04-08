9babf055b44b729a2584273f5a3e628488ece419
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-04-08 11:50:30 -0400

    one more comment

--- src/RobotTask.java
+++ src/RobotTask.java
@@ -3,6 +3,7 @@ import becker.robots.*;
 public class RobotTask {
 
     public void run() {
+        //change the path name or choose different map file 
         City maze = new City("../WalkRectangle.wld");
         Point robot_p = Point.createRandomPoint(0, 3, 0, 10);
         WalkPolyBot bot = new WalkPolyBot(maze, robot_p.y, robot_p.x);
